5cfdd00ef848181adf2080ab406d06e00088a707
hanena79 <radandai13@gmail.com>
2026-05-03 00:29:39 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,7 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   experimental: {
+    serverComponentsExternalPackages: ['bcryptjs'],
     serverComponentsExternalPackages: ['bcryptjs', 'jose'],
   },
 }
